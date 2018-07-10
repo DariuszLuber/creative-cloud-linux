d2af1d1fe336c0633e58ea1bccea6546426b30b0
Corbin Davenport <me@corbin.io>
2018-07-10 00:14:54 -0400

    Make sure Gecko is installed in the Wine prefix

--- creativecloud.sh
+++ creativecloud.sh
@@ -12,7 +12,7 @@
 source "$PLAYONLINUX/lib/sources"
 
 PREFIX="CreativeCloudDev"
-WINEVERSION="3.9"
+WINEVERSION="3.11"
 TITLE="Adobe Creative Cloud"
 EDITOR="Adobe Systems Inc."
 GAME_URL="http://www.adobe.com"
@@ -31,21 +31,28 @@ POL_SetupWindow_presentation "$TITLE" "$EDITOR" "$GAME_URL" "$AUTHOR" "$PREFIX"
 POL_System_SetArch "amd64"
 POL_Wine_SelectPrefix "$PREFIX"
 POL_Wine_PrefixCreate "$WINEVERSION"
-POL_System_TmpCreate "AppManagerTmp"
+POL_System_TmpCreate "CreativeCloud"
 Set_OS "win7"
 
 # Install dependencies
 POL_SetupWindow_wait "Please wait while msxml3 is installed..." "$TITLE"
 POL_Call POL_Install_msxml3
+POL_Call POL_Install_gdiplus
 cd "$POL_System_TmpDir"
 # Use winetricks, since the POL_corefonts version does not work with the installer
-POL_Download_Resource  "https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks"
-POL_SetupWindow_wait "Please wait while winetricks is installed... (this might take a few minutes)" "$TITLE"
+POL_Download_Resource "https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks"
+POL_SetupWindow_wait "Please wait while winetricks is installed. This might take a few minutes." "$TITLE"
 chmod +x winetricks
-./winetricks atmlib corefonts wininet winhttp gecko fontsmooth=rgb
+./winetricks atmlib corefonts fontsmooth=rgb
+
+# Download Gecko for 64-bit Wine
+cd "$POL_System_TmpDir"
+POL_Download "http://dl.winehq.org/wine/wine-gecko/2.47/wine_gecko-2.47-x86_64.msi"
+POL_SetupWindow_wait "Please wait while Gecko is installed."
+POL_SetupWindow_message "$(eval_gettext 'hold up')" "$TITLE"
+POL_AutoWine "$POL_System_TmpDir/wine_gecko-2.47-x86_64.msi"
 
 # Get the installer
-POL_SetupWindow_wait "Download in progress..." "$TITLE"
 cd "$POL_System_TmpDir"
 POL_Download "http://ccmdl.adobe.com/AdobeProducts/KCCC/1/win32/ACCCx4_5_0_331.zip"
 POL_SetupWindow_wait "Please wait while the installer is extracted..." "$TITLE"
@@ -53,6 +60,7 @@ unzip *.zip
 INSTALLER="$POL_System_TmpDir/Set-up.exe"
   
 # Run the installer
+Set_OS "win7"
 POL_Wine_WaitBefore "$TITLE"
 POL_Wine "$INSTALLER"
 POL_Shortcut "Creative Cloud.exe" "Adobe Creative Cloud"
