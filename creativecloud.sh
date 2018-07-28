517662d4c7a9968b7da8db09b249790704289983
Corbin Davenport <me@corbin.io>
2018-07-27 23:59:58 -0400

    Update to Wine 3.13, install IE8 before Creative Cloud

--- creativecloud.sh
+++ creativecloud.sh
@@ -12,7 +12,7 @@
 source "$PLAYONLINUX/lib/sources"
 
 PREFIX="CreativeCloudDev"
-WINEVERSION="3.11"
+WINEVERSION="3.13"
 TITLE="Adobe Creative Cloud"
 EDITOR="Adobe Systems Inc."
 GAME_URL="http://www.adobe.com"
@@ -28,33 +28,22 @@ POL_Debug_Init
 POL_SetupWindow_presentation "$TITLE" "$EDITOR" "$GAME_URL" "$AUTHOR" "$PREFIX"
 
 # Create prefix and temporary download folder
-POL_System_SetArch "amd64"
 POL_Wine_SelectPrefix "$PREFIX"
 POL_Wine_PrefixCreate "$WINEVERSION"
 POL_System_TmpCreate "CreativeCloud"
 Set_OS "win7"
 
 # Install dependencies
-POL_SetupWindow_wait "Please wait while msxml3 is installed..." "$TITLE"
-POL_Call POL_Install_msxml3
-POL_Call POL_Install_gdiplus
 cd "$POL_System_TmpDir"
 # Use winetricks, since the POL_corefonts version does not work with the installer
-POL_Download_Resource "https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks"
-POL_SetupWindow_wait "Please wait while winetricks is installed. This might take a few minutes." "$TITLE"
-chmod +x winetricks
-./winetricks atmlib corefonts fontsmooth=rgb
-
-# Download Gecko for 64-bit Wine
-cd "$POL_System_TmpDir"
-POL_Download "http://dl.winehq.org/wine/wine-gecko/2.47/wine_gecko-2.47-x86_64.msi"
-POL_SetupWindow_wait "Please wait while Gecko is installed."
-POL_SetupWindow_message "$(eval_gettext 'hold up')" "$TITLE"
-POL_AutoWine "$POL_System_TmpDir/wine_gecko-2.47-x86_64.msi"
+POL_Download_Resource  "https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks"
+POL_SetupWindow_wait "Please wait while winetricks is installed... (this might take a few minutes)" "$TITLE"
+chmod +x winetricks 
+./winetricks msxml3 atmlib corefonts fontsmooth=rgb wininet winhttp ie8
 
 # Get the installer
 cd "$POL_System_TmpDir"
-POL_Download "http://ccmdl.adobe.com/AdobeProducts/KCCC/1/win32/ACCCx4_5_0_331.zip"
+POL_Download "http://ccmdl.adobe.com/AdobeProducts/KCCC/1/win32/ACCCx4_6_0_391.zip"
 POL_SetupWindow_wait "Please wait while the installer is extracted..." "$TITLE"
 unzip *.zip
 INSTALLER="$POL_System_TmpDir/Set-up.exe"
@@ -67,7 +56,7 @@ POL_Shortcut "Creative Cloud.exe" "Adobe Creative Cloud"
 POL_System_TmpDelete
 
 # All done
-POL_SetupWindow_message "$(eval_gettext 'The installation is now complete, you can now use the Adobe Creative Cloud manager to download the applications you need.\n\nNOTE: The Creative Cloud manager takes a while to log in. That is completely normal.\n\nAfter you download an app, you can add a PlayOnLinux shortcut for it by clicking ADOBE CREATIVE CLOUD in the app list, clicking CONFIGURE, and clicking MAKE A NEW SHORTCUT FROM THIS VIRTUAL DRIVE. Then look for the app, like Photoshop.exe, and add it.')" "$TITLE"
+POL_SetupWindow_message "$(eval_gettext 'The installation is now complete, you can now use the Adobe Creative Cloud manager to download the applications you need.\n\nNOTE: The Creative Cloud manager takes a while to log in, and you may see an error meessage. That is completely normal - don't close the login window!\n\nAfter you download an app, you can add a PlayOnLinux shortcut for it by clicking ADOBE CREATIVE CLOUD in the app list, clicking CONFIGURE, and clicking MAKE A NEW SHORTCUT FROM THIS VIRTUAL DRIVE. Then look for the app, like Photoshop.exe, and add it.')" "$TITLE"
  
 POL_SetupWindow_Close
 exit 0
